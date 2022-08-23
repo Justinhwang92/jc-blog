2809749c2992b426806683626d8dcbb6d3b684d8
Justin <justinhwang92@gmail.com>
2022-08-23 16:30:08 -0400

    Add Links between each components

--- client/src/App.js
+++ client/src/App.js
@@ -5,16 +5,31 @@ import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import Settings from "./pages/settings/Settings";
 import Write from "./pages/write/Write";
+
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 function App() {
+  const user = false;
+
   return (
-    <>
+    <Router>
       <TopBar />
-      {/* <Homepage /> */}
-      {/* <Write /> */}
-      {/* <Settings /> */}
-      {/* <Login /> */}
-      <Register />
-    </>
+      <Routes>
+        <Route exact path="/" element={<Homepage />} />
+        <Route
+          exact
+          path="/register"
+          element={user ? <Homepage /> : <Register />}
+        />
+        <Route exact path="/login" element={user ? <Homepage /> : <Login />} />
+        <Route exact path="/write" element={user ? <Write /> : <Register />} />
+        <Route
+          exact
+          path="/settings"
+          element={user ? <Settings /> : <Register />}
+        />
+        <Route exact path="/post/:postId" element={<SinglePost />} />
+      </Routes>
+    </Router>
   );
 }
 
--- client/src/pages/homepage/Homepage.jsx
+++ client/src/pages/homepage/Homepage.jsx
@@ -9,7 +9,6 @@ export default function Homepage() {
     <>
       <Header />
       <div className="home">
-        {/* <Posts /> */}
         <SinglePost />
         <Sidebar />
       </div>
