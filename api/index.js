06b0a68323d9b17b3dca0f3ab77c756f3b45ce47
Justin <justinhwang92@gmail.com>
2022-08-24 10:00:24 -0400

    Setup MongoDB

---
+++ api/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
+
+// app.use("/", (req, res) => {
+//   console.log("main url here");
+// });
+
+app.listen("4000", () => {
+  console.log("Backend is running.");
+});
