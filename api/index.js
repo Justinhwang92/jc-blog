706ebb7cc02e2a3ce8e38992340943f28295e938
Justin <justinhwang92@gmail.com>
2022-08-24 10:42:35 -0400

    Add login and resgister api

--- api/index.js
+++ api/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
+app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO_URL, {
@@ -13,9 +15,7 @@ mongoose
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
 
-// app.use("/", (req, res) => {
-//   console.log("main url here");
-// });
+app.use("/api/auth", authRoute);
 
 app.listen("4000", () => {
   console.log("Backend is running.");
---
+++ api/routes/auth.js
@@ -0,0 +1,53 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+
+// Register
+router.post("/register", async (req, res) => {
+  try {
+    // hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPass = await bcrypt.hash(req.body.password, salt);
+
+    // user model
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPass,
+    });
+
+    // save user to database
+    const user = await newUser.save();
+    // send user data
+    res.status(200).json(user);
+  } catch (error) {
+    // server error
+    res.status(500).send(error);
+  }
+});
+
+// Login
+router.post("/login", async (req, res) => {
+  try {
+    // find user by email
+    const user = await User.findOne({
+      username: req.body.username,
+    });
+    // check if user exists
+    !user && res.status(400).json("Wrong credentials!");
+
+    // check password
+    const validate = await bcrypt.compare(req.body.password, user.password);
+    // check if password is valid
+    !validate && res.status(400).json("Wrong credentials!");
+
+    // send user data without password
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch (error) {
+    // server error
+    res.status(500).send(error);
+  }
+});
+
+module.exports = router;
