f8ded66f4dba09ab46179eb2e4a09d63d11b88e6
Justin <justinhwang92@gmail.com>
2022-08-25 10:39:03 -0400

    Add fetching all posts

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,23 +1,31 @@
 import "./post.css";
+import { Link } from "react-router-dom";
 
-export default function Post() {
+export default function Post({ post }) {
   return (
     <div className="post">
-      <img
-        className="postImg"
-        src="https://images.unsplash.com/photo-1653052041059-f300495ab0ea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
-        alt=""
-      />
+      {/* Image */}
+      {post.photo && <img className="postImg" src={post.photo} alt="" />}
       <div className="postInfo">
+        {/* Categories */}
         <div className="postCats">
-          <span className="postCat">Music</span>
-          <span className="postCat">Life</span>
+          {post.categories.map((cat) => (
+            <span className="postCat">{cat.name}</span>
+          ))}
         </div>
-        <span className="postTitle">post title placeholder</span>
+        {/* Title */}
+        <Link to={`/post/${post._id}`} className="link">
+          <span className="postTitle">{post.title}</span>{" "}
+        </Link>
+
         <hr />
-        <span className="postDate">1 hour ago</span>
+        {/* Date */}
+        <span className="postDate">
+          {new Date(post.createdAt).toDateString()}
+        </span>
       </div>
-      <p className="postDesc">post description placeholder</p>
+      {/* Description */}
+      <p className="postDesc">{post.desc}</p>
     </div>
   );
 }
--- client/src/pages/homepage/Homepage.jsx
+++ client/src/pages/homepage/Homepage.jsx
@@ -1,15 +1,27 @@
+import { useEffect, useState } from "react";
 import Header from "../../components/header/Header";
 import Posts from "../../components/posts/Posts";
 import Sidebar from "../../components/sidebar/Sidebar";
 import SinglePost from "../../components/singlePost/SinglePost";
 import "./homepage.css";
+import axios from "axios";
 
 export default function Homepage() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get("/posts");
+      setPosts(res.data);
+    };
+    fetchPosts();
+  }, []);
+
   return (
     <>
       <Header />
       <div className="home">
-        <SinglePost />
+        <Posts posts={posts} />
         <Sidebar />
       </div>
     </>
